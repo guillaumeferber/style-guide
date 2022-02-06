7b25520092d0a4fe0b3506284ba7db19b191e687
Guillaume Ferber <ferber.g@gmail.com>
2022-02-06 17:34:11 -0500

    Update gulpfile & package.json

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const { src, dest, parallel, watch, series } = require('gulp');
 const connect = require('gulp-connect');
 const open = require('gulp-open');
 // CSS-Sass
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const nano = require('gulp-cssnano');
 const uncss = require('gulp-uncss');
 // JS
@@ -37,8 +37,7 @@ const configuration = {
       'scripts/**/*.js'
     ],
     css: [
-      "node_modules/bootstrap/scss/bootstrap.scss",
-      'scss/styles.scss',
+      'scss/style.scss',
       'html/components/**/**/*.scss'
     ]
   },
@@ -48,7 +47,8 @@ const configuration = {
       css: 'scss/',
       js: 'scripts/'
     },
-    dest: './dist/'
+    dest: './dist/',
+    demo: './dist/demo/'
   },
   server: {
     port: 8001
@@ -57,13 +57,13 @@ const configuration = {
 
 const connectServer = () => {
   return connect.server({
-    root: 'dist',
+    root: 'dist/demo',
     port: configuration.server.port,
     livereload: true
   });
 }
 const openServer = () => {
-  return src('dist/index.html')
+  return src('dist/demo/index.html')
     .pipe(open({uri: 'http://localhost:' + configuration.server.port + '/'}));
 }
 
@@ -73,32 +73,32 @@ const copyFolder = () => {
 
 const html = {
   validate() {
-    return src(configuration.folders.dest + '**/*' + configuration.extension.html)
+    return src(configuration.folders.demo + '**/*' + configuration.extension.html)
       .pipe(htmlValidator({skipWarnings: true}))
       .pipe(htmlValidator.reporter())
       .pipe(connect.reload());
   },
   prettify() {
-    return src(configuration.folders.dest + '*' + configuration.extension.html)
+    return src(configuration.folders.demo + '*' + configuration.extension.html)
     .pipe(prettify({indent_char: ' ', indent_size: 2}))
-    .pipe(dest(configuration.folders.dest))
+    .pipe(dest(configuration.folders.demo))
     .pipe(connect.reload());
   },
   extend() {
     return src(configuration.folders.src.html + '**/*' + configuration.extension.html)
         .pipe(extender({annotations:true,verbose:false, root: './'})) // default options
-        .pipe(dest(configuration.folders.dest))
+        .pipe(dest(configuration.folders.demo))
         .pipe(connect.reload());
   },
   htmlClean() {
     return del([
-      `${configuration.folders.dest}base`,
-      `${configuration.folders.dest}components`
+      `${configuration.folders.demo}base`,
+      `${configuration.folders.demo}components`
     ]);
   },
   destClean() {
     return del([
-      `${configuration.folders.dest}/**/*`
+      `${configuration.folders.dest}`
     ]);
   },
 
@@ -109,24 +109,24 @@ const css = () => {
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(concat('style.min' + configuration.extension.css))
     .pipe(nano())
-    .pipe(dest(configuration.folders.dest))
+    .pipe(dest(configuration.folders.demo))
     .pipe(connect.reload());
 }
 
 const unCss = () => {
-  return src(configuration.folders.dest + 'style.min' + configuration.extension.css)
+  return src(configuration.folders.demo + 'style.min' + configuration.extension.css)
   .pipe(uncss({
-    html: [configuration.folders.dest + '/**/*.html']
+    html: [configuration.folders.demo + '/**/*.html']
   }))
   .pipe(nano())
-  .pipe(dest(configuration.folders.dest))
+  .pipe(dest(configuration.folders.demo))
 }
 
 const js = () => {
   return src(configuration.assets.js)
     .pipe(concat('app.min' + configuration.extension.js))
     .pipe(uglify())
-    .pipe(dest(configuration.folders.dest ))
+    .pipe(dest(configuration.folders.demo))
     .pipe(connect.reload());
 }
 
@@ -141,8 +141,13 @@ const copyConfigFiles = () => {
   return src(configuration.configFiles).pipe(dest(configuration.folders.dest));
 }
 
+const copyScssFiles = () => {
+  return src(configuration.folders.src.css + '**/*').pipe(dest(configuration.folders.dest + configuration.folders.src.css));
+}
+
 exports.html = series(html.prettify, html.extend, html.htmlClean);
 
 exports.default = series(html.destClean, parallel(css, js, this.html), unCss, copyFolder);
 exports.watch = series(this.default, parallel(connectServer, watchAssets, openServer));
-exports.build = series(html.destClean, parallel(css, js), this.html, unCss, copyFolder, copyConfigFiles);
+exports.build = series(html.destClean, parallel(css, js), this.html, unCss, copyFolder, copyConfigFiles, copyScssFiles);
+exports.deploy = series(html.destClean, parallel(css, js), copyFolder, copyConfigFiles, copyScssFiles);
